ac9d765832ed1b2b79d94161a2bda898db50120d
Subha-CJPR <56904625+Subha-CJPR@users.noreply.github.com>
2023-06-29 18:48:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 //your JS code here. If required.
+function getHelloWorldPromise() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve("Hello, world!");
+    }, 1000);
+  });
+}
+
+const outputElement = document.getElementById("output");
+
+getHelloWorldPromise()
+  .then(text => {
+    outputElement.innerHTML = text;
+  });
